300081234218cc37161cacccedb5dd035a6fb117
OkonoWil <okonowilfried@gmail.com>
2022-11-10 04:27:49 +0100

    add tailwind css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        @vite('resources/css/app.css')
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -125,6 +126,9 @@
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
+                    <h3 class="text-3xl font-bold underline">
+                        Hello world!
+                      </h3>
                 </div>
             </div>
         </div>

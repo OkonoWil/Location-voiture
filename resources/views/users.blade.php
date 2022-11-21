04577e6d5ee343767245f17ca0c0b7d4def85978
OkonoWil <okonowilfried@gmail.com>
2022-11-21 01:32:10 +0100

    delete all

--- resources/views/users.blade.php
+++
@@ -1,5 +0,0 @@
-@extends("layouts.master")
-
-@section("contenu")
-<h1>Liste des utilisateurs</h1>
-@endsection
--- resources/views/welcome.blade.php
+++
@@ -1,5 +0,0 @@
-@extends("layouts.master")
-
-@section("contenu")
-<livewire:counter />
-@endsection
